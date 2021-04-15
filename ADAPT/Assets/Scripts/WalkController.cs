c9d55e0f76b06f786cd0e663d10f7499a8200a52
Graham <leegrah@sheridancollege.ca>
2021-04-15 13:06:17 -0400

    Added player prefab

--- ADAPT/Assets/Scripts/WalkController.cs
+++ ADAPT/Assets/Scripts/WalkController.cs
@@ -10,11 +10,16 @@ public class WalkController : MonoBehaviour
     private float facing = 1;
     private bool grounded = false;
     private bool jumping = false;
+    private bool dashing = false;
 
     [SerializeField]
     private float m_walkAcceleration;
     [SerializeField]
     private float m_maxWalkSpeed;
+    [SerializeField]
+    private float m_dashSpeed;
+    [SerializeField]
+    private float m_dashDuration;
     [SerializeField]
     private float m_jumpForce;
     [SerializeField]
@@ -39,48 +44,80 @@ public class WalkController : MonoBehaviour
     {
         // Gravity & Jump
         grounded = false;
-        if (Physics.Raycast(transform.position, -transform.up, m_capsuleCollider.height / 2 + 0.1f)) {
+        if (Physics.Raycast(transform.position, -transform.up, m_capsuleCollider.height / 2 + 0.1f))
+        {
             grounded = true;
         }
-        if (Input.GetKeyDown(KeyCode.Z) && grounded) {
+        if (Input.GetKeyDown(KeyCode.Z) && grounded && !dashing)
+        {
             velocity.y += m_jumpForce;
             grounded = false;
             jumping = true;
         }
-        if (!Input.GetKey(KeyCode.Z)) {
+        if (!Input.GetKey(KeyCode.Z))
+        {
             jumping = false;
         }
-        if (!grounded) {
-            if (jumping) {
+        if (!grounded && !dashing)
+        {
+            if (jumping)
+            {
                 velocity.y = Mathf.Clamp(velocity.y - m_jumpGravity, -m_terminalVelocity, m_terminalVelocity);
             }
-            else {
+            else
+            {
                 velocity.y = Mathf.Clamp(velocity.y - m_baseGravity, -m_terminalVelocity, m_terminalVelocity);
             }
         }
 
         // Horizontal Movement
-        if (m_deadZone < Mathf.Abs(Input.GetAxis("Horizontal")))
+        if (m_deadZone < Mathf.Abs(Input.GetAxis("Horizontal")) && !dashing)
         {
             velocity.x = Mathf.Clamp(velocity.x + Mathf.Sign(Input.GetAxis("Horizontal")) * m_walkAcceleration, -m_maxWalkSpeed, m_maxWalkSpeed);
             facing = Mathf.Sign(Input.GetAxis("Horizontal"));
         }
-        else if (m_deceleration <= Mathf.Abs(velocity.x)) {
+        else if (m_deceleration <= Mathf.Abs(velocity.x))
+        {
             velocity.x -= Mathf.Sign(velocity.x) * m_deceleration;
-        } else {
+        }
+        else
+        {
             velocity.x = 0;
+        }
+
+        //Dash control
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            dashing = true;
+            StartCoroutine(Dash());
         }
 
         // Nullifying velocity when colliding with objects (stops player from sticking to walls, being able to jump around ceilings, etc.)
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Mathf.Sign(velocity.x) * transform.right, out hit, m_capsuleCollider.radius)) {
+        if (Physics.Raycast(transform.position, Mathf.Sign(velocity.x) * transform.right, out hit, m_capsuleCollider.radius + 0.1f))
+        {
             velocity.x = 0;
         }
-        if (Physics.Raycast(transform.position, Mathf.Sign(velocity.y) * transform.up, m_capsuleCollider.height / 2)) {
+        if (Physics.Raycast(transform.position, Mathf.Sign(velocity.y) * transform.up, m_capsuleCollider.height / 2 + 0.1f))
+        {
             velocity.y = 0;
         }
 
         // Applying velocity to Rigidbody
         m_rigidBody.velocity = velocity;
+
+        IEnumerator Dash()
+        {
+            // Dash
+
+            velocity.y = 0;
+            float dashDir = facing;
+            for (float i = 0; i < m_dashDuration; i += Time.deltaTime)
+            {
+                velocity.x += dashDir * (m_dashSpeed - i / m_dashDuration * m_dashSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+            dashing = false;
+        }
     }
 }
