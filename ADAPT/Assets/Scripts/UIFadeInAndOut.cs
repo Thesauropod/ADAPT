bcc5522527e7d261110721e007685a77201ae9e5
Slick0117 <57147401+Slick0117@users.noreply.github.com>
2021-04-16 18:08:11 -0400

    UI Fade In and Out Script
    Create the script for fading sprites in and out

---
+++ ADAPT/Assets/Scripts/UIFadeInAndOut.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFadeInAndOut : MonoBehaviour
+{
+
+    public SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+    }
+
+    IEnumerator FadeIn()
+    {
+        for(float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        for(float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void startFadeIn()
+    {
+        StartCoroutine("FadeIn");
+    }
+
+    public void startFadeOut()
+    {
+        StartCoroutine("FadeOut");
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine("FadeIn");
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine("FadeOut");
+        }
+    }
+}
