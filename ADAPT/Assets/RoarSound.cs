0e9d1b2d5abe599eb11c7048d26518c13f2055a9
KC-LC <kc971201@gmail.com>
2021-04-18 14:38:32 -0400

    Fmod

---
+++ ADAPT/Assets/RoarSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoarSound : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance Roar;
+
+    private void Start()
+    {
+        Roar = FMODUnity.RuntimeManager.CreateInstance("event:/MainCharacter/Roar");
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            playSound();
+
+            //FMODUnity.RuntimeManager.PlayOneShot("event:/MainCharacter/Roar", transform.position); }
+
+        }
+
+        void playSound()
+        {
+            FMOD.Studio.PLAYBACK_STATE fmodpbState;
+            Roar.getPlaybackState(out fmodpbState);
+            if (fmodpbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                Roar.start();
+            }
+
+        }
+
+
+    }
+}
