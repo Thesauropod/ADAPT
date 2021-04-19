25f96afce5ebc8541d382c77793ec40bb9961699
EmperorAether <daviseth@sheridancollege.ca>
2021-04-19 15:44:38 -0400

    Created breakable wall prefab

---
+++ ADAPT/Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour
+{
+    //Get components
+    Collider2D myCollider;
+
+    SpriteRenderer mySprite;
+
+    public Sprite brokenImage;
+
+    public GameObject brokenPiece;
+
+    //Set Variables
+    public bool isDestroyed;
+    void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+        mySprite = GetComponent<SpriteRenderer>();
+
+        isDestroyed = false;
+    }
+
+    private void Update()
+    {
+        if (isDestroyed)
+        {
+            Broken();
+        }
+    }
+
+
+    void Broken()
+    {
+        myCollider.enabled = false;
+        mySprite.sprite = brokenImage;
+        brokenPiece.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
