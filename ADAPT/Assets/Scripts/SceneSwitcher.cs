81263165eb27b63c740d7287897ef2c48e3709ee
pengyiy <pengyiy@sheridancollege.ca>
2021-04-21 23:16:28 -0400

    Start and End Screen

---
+++ ADAPT/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+  public void start()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void end()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
